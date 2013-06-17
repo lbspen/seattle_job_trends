b20bde5b1f7f1a2324f0cbd5da0bdfa88137e018
lbs <lbspen@gmail.com>
2013-06-16 23:59:09 -0700

    added rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeattleJobTrends::Application.config.secret_token = '07e7700deb5d109e2d2d2012fc38433421932a50d0eeea30ad6055777671bb4e4518ad53d36b86777d33a3a2515908343c926ad2d5622445d780244c43dabe6b'
